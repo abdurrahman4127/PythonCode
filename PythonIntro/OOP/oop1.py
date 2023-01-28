7c9ee5e292387e809445a9a113b316fd1905777d
Abdur Rahman <whoamiiammr.nobody@gmail.com>
2023-01-28 14:43:33 +0600

    OOP code adding

---
+++ PythonIntro/OOP/oop1.py
@@ -0,0 +1,24 @@
+# instance variables
+
+class Employee:
+    pass       # to leave empty for a time, use 'pass'
+
+# creat instance of cRahmanlass  
+emp1 = Employee()
+emp2 = Employee()
+
+# both have unique memory address
+print(emp1)  
+print(emp2)
+
+# 
+emp1.first = "Abdur"
+emp1.last = "Rahman"
+emp1.email = "whoamiiammr.nobody@gmail.com"
+
+emp2.first = "l"
+emp2.last = "lawliet"
+emp2.email = "llawliet@gmail.com"
+
+print(emp1.email)
+print(emp2.email)
